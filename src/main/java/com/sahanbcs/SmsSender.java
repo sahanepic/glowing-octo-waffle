2d1d07dbb37bbda4eed38ad626d863f2afb20985
sahanbcs <sahanepic@gmail.com>
2021-04-20 04:04:52 +0530

    first commit towillo

---
+++ src/main/java/com/sahanbcs/SmsSender.java
@@ -0,0 +1,10 @@
+package com.sahanbcs;
+
+
+
+public interface SmsSender {
+
+    void sendSms(SmsRequest smsRequest);
+
+    // or maybe void sendSms(String phoneNumber, String message);
+}
---
+++ src/main/java/com/sahanbcs/TowilioSmsApplication.java
@@ -0,0 +1,13 @@
+package com.sahanbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TowilioSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TowilioSmsApplication.class, args);
+	}
+
+}
